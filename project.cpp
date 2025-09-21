b190f8514cf33d25cf491612151379f4bf075822
M0hamed-Abdo <moh3m7ed@gmail.com>
2025-09-21 10:41:50 +0300

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -15,36 +15,19 @@ public:
 		cout << "AGE :" << age << endl;
 		cout << "SALARY :" << salary << endl;
 	}
-	void set_id(int new_id) {
+	Employee(int new_id) {
 		id = new_id;
 	}
 	int get_id() {
 		return id;
 	}
 };
-class Teacher : public Employee{
+class Teacher : public Employee {
 private:
 	string subject;
 public:
-	void record_employee() override{
-		cout << "Enter Name :";
-		cin >> name;
-		cout << "Enter Age :";
-		cin >> age;
-		cout << "Enter Salary :";
-		cin >> salary;
-		cout << "Enter Teacher Subject :";
-		cin >> subject;
-	}
-	void display_employee() override{
-		Employee::display_employee();
-		cout << "Teacher Subject :" << subject << endl;
-	}
-};
-class Engineer : public Employee{
-private:
-	string engineer_type;
-public:
+	Teacher(int new_id) : Employee(new_id) {}
+
 	void record_employee() override {
 		cout << "Enter Name :";
 		cin >> name;
@@ -52,125 +35,165 @@ public:
 		cin >> age;
 		cout << "Enter Salary :";
 		cin >> salary;
-		cout << "Engineering Specialization :";
+		cout << "Enter Subject :";
+		cin >> subject;
+	}
+	void display_employee() override {
+		cout << "-----Teacher-----" << endl;
+		Employee::display_employee();
+		cout << "Subject :" << subject << endl;
+	}
+};
+class Engineer : public Employee {
+private:
+	string engineer_type;
+public:
+	Engineer(int new_id) : Employee(new_id) {}
+	void record_employee() override {
+		cout << "Enter Name :";
+		cin >> name;
+		cout << "Enter Age :";
+		cin >> age;
+		cout << "Enter Salary :";
+		cin >> salary;
+		cout << "Specialization :";
 		cin >> engineer_type;
 	}
 	void display_employee() override {
+		cout << "-----Engineer-----" << endl;
 		Employee::display_employee();
 		cout << "Engineering Specialization :" << engineer_type << endl;
 	}
 };
-
+class Manager {
+private:
 vector<Employee*>emp;
-bool add_teacher() {
-	cout << "------RECORD #" << emp.size() + 1 << "------" << endl;
-	int new_id;
-	cout << "enter id :";
-	cin >> new_id;
-	for (int i = 0; i < emp.size(); i++) {
-		if (new_id == emp[i]->get_id()) {
-			cout << "This ID already used " << endl;
-			return false;
+public:
+bool check_id(int id) {
+		for (int i = 0; i < emp.size(); i++) {
+			if (emp[i]->get_id() == id) {
+				return false;
+			}
 		}
+		return true;
 	}
-	emp.push_back(new Teacher);
-	emp.back() ->set_id(new_id);
-	emp.back()->record_employee();
-	cout << "Done Record" << endl;
-	return true;
-}
-bool add_engineer() {
-	cout << "------RECORD #" << emp.size() + 1 << "------" << endl;
-	int new_id;
-	cout << "enter id :";
-	cin >> new_id;
-	for (int i = 0; i < emp.size(); i++) {
-		if (new_id == emp[i]->get_id()) {
-			cout << "This ID already used " << endl;
-			return false;
-		}
+bool employee_empty() {
+	if (emp.empty()) {
+		cout << "-----No Employee Record-----" << endl;
+		return true;
 	}
-	emp.push_back(new Engineer);
-	emp.back()->set_id(new_id);
-	emp.back()->record_employee();
-	cout << "Done Record" << endl;
-	return true;
+	return false;
 }
-
 void add_employee() {
 	cout << "\n";
 	while (1) {
 		int employee_type;
 		cout << "[1]Add teacher" << endl;
 		cout << "[2]Add Engineer" << endl;
-		cout << "[3]cancl\n" << endl;
+		cout << "[3]Cancel\n" << endl;
 		cout << "ENTER TYPE :";
 		cin >> employee_type;
-		if (employee_type == 1) {
-			add_teacher();
-			return;
+		if (employee_type != 1 && employee_type != 2 && employee_type != 3) {
+			cout << "woring choiec" << endl;
+			continue;
 		}
-		else if (employee_type == 2) {
-			add_engineer();
+		else if (employee_type == 3) {
+			cout << "Cancelled by user "<< endl;
 			return;
 
 		}
-		else if (employee_type == 3) {
+		int new_id;
+		while (true) {
+			cout << "------RECORD #" << emp.size() + 1 << "------" << endl;
+			cout << "enter id :";
+			cin >> new_id;
+			if (check_id(new_id) == false) {
+				cout << "This ID already used. Please try again." << endl;
+				continue;
+			}
+			break;
+		}
+		if (employee_type == 1) {
+			emp.push_back(new Teacher(new_id));
+			emp.back()->record_employee();
+			cout << "Done Record" << endl;
 			return;
 		}
-		else {
-			cout << "Wrong choice ,try agin" << endl;
+		else if (employee_type == 2) {
+			emp.push_back(new Engineer(new_id));
+			emp.back()->record_employee();
+			cout << "Done Record" << endl;
+			return;
+
 		}
 	}
 }
 void add_multi_employee() {
 	int employee_count;
 	int employee_type;
+	while (1) {
+	cout << "\n";
 	cout << "[1]Add Teacher" << endl;
 	cout << "[2]Add Engineer" << endl;
-	cout << "[3]cancl\n" << endl;
+	cout << "[3]Cancel" << endl;
+	cout << "\n";
 	cout << "ENTER TYPE :";
-	cin >> employee_type;  
-	cout <<  "\n";
+	cin >> employee_type;
+	if (employee_type != 1 && employee_type != 2 && employee_type != 3) {
+		cout << "Wrong choice" << endl;
+		continue;
+	}
+	else if (employee_type == 3) {
+		cout << "Cancelled by user " << endl;
+		return;
+	}
+	cout << "\n";
 	cout << "Enter Employee count:";
 	cin >> employee_count;
+	int new_id;
 	for (int i = 0; i < employee_count;) {
-		if(employee_type == 1){
-			if (add_teacher() == true) {
-				i++;
-			}
+		cout << "------RECORD #" << emp.size() + 1 << "------" << endl;
+		cout << "enter id :";
+		cin >> new_id;
+		if (check_id(new_id) == false) {
+			cout << "This ID already used " << endl;
+			continue;
+		}
+		if (employee_type == 1) {
+			emp.push_back(new Teacher(new_id));
+			emp.back()->record_employee();
+			cout << "Done Record" << endl;
+			i++;
 		}
 		else if (employee_type == 2) {
-			if (add_engineer() == true) {
+			emp.push_back(new Engineer(new_id));
+			emp.back()->record_employee();
+			cout << "Done Record" << endl;
 			i++;
 			}
 		}
-		else if (employee_type == 3) {
-			return;
-		}
+		return;
 	}
 }
-
-bool employee_list_is_empty() {
-	return emp.empty();
-}
 void search_by_id() {
-	if (employee_list_is_empty() == true) {
-		cout << "------NO EMPLOYEE RECORD------" << endl;
+	if (employee_empty() == true) {
 		return;
 	}
 	int id_search;
+	cout << "\n";
 	cout << "enter id for search :";
 	cin >> id_search;
 	for (int i = 0; i < emp.size(); i++) {
-		if (emp[i]->get_id() == id_search) {
+		if (id_search == emp[i]->get_id()) {
 			emp[i]->display_employee();
+			return;
 		}
 	}
+		cout << "Not Found  Employee" << endl;
+		return;
 }
 void update_by_id() {
-	if (employee_list_is_empty() == true) {
-		cout << "-----No Employee Record-----" << endl;
+	if (employee_empty() == true) {
 		return;
 	}
 	int id_update;
@@ -187,15 +210,14 @@ void update_by_id() {
 }
 void delete_employee() {
 	int id_delete;
-	if (employee_list_is_empty() == true) {
-		cout << "-----No Employee Record-----" << endl;
+	if (employee_empty() == true) {
 		return;
 	}
 	cout << "enter id for Delete:";
 	cin >> id_delete;
 	for (int i = 0; i < emp.size(); i++) {
 		if (emp[i]->get_id() == id_delete) {
-			delete emp[i];
+			delete emp[i];//
 			emp.erase(emp.begin() + i);
 			cout << "Employee Deleted Successfully" << endl;
 			return;
@@ -203,69 +225,54 @@ void delete_employee() {
 	}
 	cout << "ID Not Found" << endl;
 }
-void display_all_teacher() {
-	int count_of_teacher = 1;
-	if (employee_list_is_empty() == true) {
-		cout << "------NO EMPLOYEE RECORD------" << endl;
-	}
-	else {
-		for (auto type_emp : emp) {
-			Teacher* teacher = dynamic_cast <Teacher*>(type_emp);
-			if (teacher != nullptr) {
-				cout << "-----Teacher #" << count_of_teacher++ << "-----" << endl;
-				teacher->display_employee();
-				cout << "----------------" << endl;
-			}
-		}
-	}
-}
-void display_all_engineer() {
-	int count_of_engineer = 1;
-	if (employee_list_is_empty() == true) {
-		cout << "------NO EMPLOYEE RECORD------" << endl;
-	}
-	else {
-		for (auto type_emp : emp) {
-			Engineer* engineer = dynamic_cast <Engineer*>(type_emp);
-			if (engineer != nullptr) {
-				cout << "-----Engineer #" << count_of_engineer++ << "-----" << endl;
-				engineer->display_employee();
-				cout << "----------------" << endl;
-			}
-		}
-	}
-}
-void display_all_employee() {
-	int display_choice;
-	if (emp.size() == 0) {
-		cout << "------NO EMPLOYEE RECORD------" << endl;
+void display_all() {
+	if (employee_empty() == true) {
 		return;
 	}
+	int display_choice;
 	cout << "[1] All teacher" << endl;
 	cout << "[2] All Engineer" << endl;
 	cout << "[3] All Employee \n" << endl;
 	cout << "Enter your choice :";
 	cin >> display_choice;
-	if (display_choice == 1) {
-		display_all_teacher();
+	for (int i = 0; i < emp.size(); i++) {
+		if (display_choice == 1) {
+			Teacher* teacher = dynamic_cast<Teacher*>(emp[i]);
+			if (teacher != nullptr) {
+				teacher->display_employee();
+			}
+		}
+		else if (display_choice == 2) {
+			Engineer* engineer = dynamic_cast<Engineer*>(emp[i]);
+			if (engineer != nullptr) {
+				engineer->display_employee();
+			}
+		}
+		else if (display_choice == 3) {
+			for (auto emp_ptr : emp) {
+				Teacher* teacher = dynamic_cast<Teacher*>(emp_ptr);
+				if (teacher != nullptr) {
+					teacher->display_employee();
+					cout << "------------------" << endl;
+				}
+			}
+			for (auto emp_ptr : emp) {
+				Engineer* engineer = dynamic_cast<Engineer*>(emp_ptr);
+				if (engineer != nullptr) {
+					engineer->display_employee();
+					cout << "------------------" << endl;
+				}
+			}
+		}
+		else {
+			cout << "Wrong choice" << endl;
+			return;
+		}
 	}
-	else if (display_choice == 2) {
-		display_all_engineer();
-	}
-	else if (display_choice == 3) {
-		cout << "   Teachers Section" << endl;
-		display_all_teacher();
-		cout << "\n";
-		cout << "   Engineering Section" << endl;
-		display_all_engineer();
-		cout << "\n";
-	}
-	else {
-		cout << "Woring choice " << endl;
-		return;
-	}
+
 }
 void menue() {
+
 	int choice;
 	while (1) {
 		cout << "\n";
@@ -277,9 +284,9 @@ void menue() {
 		cout << "[5] search by ID" << endl;
 		cout << "[6] Display All Employee" << endl;
 		cout << "[0] Exit" << endl;
+		cout << "\n";
 		cout << "Enter your choice :";
 		cin >> choice;
-
 		switch (choice) {
 		case 1:
 			add_employee();
@@ -297,7 +304,7 @@ void menue() {
 			search_by_id();
 			break;
 		case 6:
-			display_all_employee();
+			display_all();
 			break;
 		case 0:
 			cout << "Exiting....." << endl;
@@ -305,9 +312,11 @@ void menue() {
 		default:
 			cout << "the woring type" << endl;
 			break;
+			}
 		}
 	}
-}
+};
 int main() {
-	menue();
+	Manager  manager;
+	manager.menue();
 }
